1ffd354e179f8bf99a26e72706deccce474d393b
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-02 22:21:00 +0000

    Person API endpoint - expand query matching

--- src/pages/api/people/[personID].ts
+++ src/pages/api/people/[personID].ts
@@ -1,8 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type {
-  CardName,
-  CardSuite,
-} from "../../../data/Cards/cardMappings";
+import type { CardName, CardSuite } from "../../../data/Cards/cardMappings";
 import { people } from "../../../data/people";
 
 export type PersonData = {
@@ -16,12 +13,12 @@ export type PersonData = {
   gamesPlayed: CardName[];
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<PersonData | { message: string }>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<PersonData | { message: string }>) {
   const person: PersonData | undefined = people.find(
-    (person) => person.id === (req.query.personID as string)
+    (person) =>
+      person.id === (req.query.personID as string) ||
+      person.name.toLowerCase() === (req.query.personID as string).toLowerCase() ||
+      person.japaneseName.toLowerCase() === (req.query.personID as string).toLowerCase()
   );
 
   if (person) {
