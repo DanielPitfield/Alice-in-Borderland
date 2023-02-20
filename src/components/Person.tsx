7fa0db1e43aebed8b622532220760915e016cc7b
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-20 16:04:39 +0000

    Person - image and info styling (initial)

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -3,23 +3,27 @@ import styles from "../styles/Person.module.scss";
 import type { PersonData } from "../pages/api/people/[personID]";
 import PersonFacts from "./PersonFacts";
 import PersonCardGames from "./PersonCardGames";
+import Image from "next/image";
 
 export interface PersonProps {
   person: PersonData;
 }
 
-// TODO: Image (on left side), then all information (on right side)
+// TODO: Image src path
 // TODO: Maybe show game speciality next to title (would need to decouple from PersonCardGames)
 // TODO: Then information/facts/bio and list of cards/games played
 const Person = (props: PersonProps) => {
   return (
     <article className={styles.wrapper}>
-      <h1 className={styles.title}>{`${props.person.name} (${props.person.japaneseName})`}</h1>
-      <PersonFacts person={props.person} />
-      <PersonCardGames
-        gameSpeciality={props.person.gameSpeciality}
-        gamesPlayed={props.person.gamesPlayed}
-      />
+      <div className={styles.imageWrapper}>
+        <Image className={styles.image} src={"/Images/usagi.gif"} alt={props.person.name} width={600} height={600} />
+      </div>
+
+      <div className={styles.info}>
+        <h1 className={styles.title}>{`${props.person.name} (${props.person.japaneseName})`}</h1>
+        <PersonFacts person={props.person} />
+        <PersonCardGames gameSpeciality={props.person.gameSpeciality} gamesPlayed={props.person.gamesPlayed} />
+      </div>
     </article>
   );
 };
