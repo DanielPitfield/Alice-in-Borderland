8abae81a1d9824ace83a539c66b1a5672f9314be
Benj <benj>
2023-02-22 20:47:47 +0000

    Kuina image

--- src/data/People/PersonImageMappings.ts
+++ src/data/People/PersonImageMappings.ts
@@ -2,5 +2,6 @@ import type { StaticImageData } from "next/image";
 import type { PersonName } from "./AllPeople";
 
 import Usagi from "../../../public/Images/Usagi.gif";
+import Kuina from "../../../public/Images/Kuina.jpg";
 
-export const PersonImageMappings: { name: PersonName; image: StaticImageData }[] = [{ name: "Usagi", image: Usagi }];
+export const PersonImageMappings: { name: PersonName; image: StaticImageData }[] = [{ name: "Usagi", image: Usagi }, {name: "Kuina", image: Kuina}];
