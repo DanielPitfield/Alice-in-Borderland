a11d005374a16df931f643416295b7d93a4d025c
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-23 02:10:54 +0000

    cards page styling

--- src/pages/cards.tsx
+++ src/pages/cards.tsx
@@ -1,5 +1,11 @@
+import styles from "../styles/CardWall.module.scss";
+
 import CardWall from "../components/CardWall";
 
 export default function CardWallPage() {
-  return <CardWall />;
+  return (
+    <section className={styles.wrapper}>
+      <CardWall />
+    </section>
+  );
 }
