2238ed6320923e5e1d4b543920bd8e8685d6a040
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-25 03:10:35 +0000

    Fix when showing multiple citizens

--- src/components/GamePlayers.tsx
+++ src/components/GamePlayers.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Game.module.scss";
 import type { GameData } from "../pages/api/games/[gameID]";
 import Link from "next/link";
 import { PersonNames } from "../data/People/AllPeople";
+import type { CardName } from "../data/Cards/AllCards";
 
 interface GamePlayersProps {
   players: GameData["players"];
@@ -12,6 +13,29 @@ interface GamePlayersProps {
 
 const GamePlayers = (props: GamePlayersProps) => {
   // TODO: Should the labels for the different properties be <span>, <strong> or text within the <div>?
+
+  // What is the path to the citizen's profile page (if they have one)?
+  function getCitizenPagePath(citizen: `${string} - ${CardName}`): string | null {
+    // The citizen's name comes before the dash
+    const citizenName: string | undefined = citizen.split("-")[0];
+
+    if (!citizenName) {
+      return null;
+    }
+
+    // Does the citizen have a person profile page?
+    return PersonNames.find((personName) => citizenName.split(" ").some((x) => x === personName)) ?? null;
+  }
+
+  // The citizens of the game which also have their own person profile page
+  const citizensWithProfiles: GameData["citizens"] = props.citizens?.filter((citizen) => {
+    return getCitizenPagePath(citizen);
+  });
+
+  const otherCitizens: GameData["citizens"] = props.citizens?.filter((citizen) => {
+    return getCitizenPagePath(citizen) === null;
+  });
+
   return (
     <>
       {props.players.length > 0 && (
@@ -32,18 +56,8 @@ const GamePlayers = (props: GamePlayersProps) => {
       {props.citizens && (
         <div>
           <span>Citizens: </span>
-          {props.citizens.map((citizen) => {
-            // The citizen's name comes before the dash
-            const citizenName: string | undefined = citizen.split("-")[0];
-
-            if (!citizenName) {
-              return citizen;
-            }
-
-            // Does the citizen have a person profile page?
-            const pagePath: string | undefined = PersonNames.find((personName) =>
-              citizenName.split(" ").some((x) => x === personName)
-            );
+          {citizensWithProfiles?.map((citizen) => {
+            const pagePath = getCitizenPagePath(citizen);
 
             if (!pagePath) {
               return citizen;
@@ -55,6 +69,8 @@ const GamePlayers = (props: GamePlayersProps) => {
               </Link>
             );
           })}
+
+          {otherCitizens?.join(", ")}
         </div>
       )}
     </>
