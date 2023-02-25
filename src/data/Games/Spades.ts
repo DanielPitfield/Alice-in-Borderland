9c97401f148e0b4f70d4368c737e673380fe275d
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-25 01:09:29 +0000

    Data - Spades (face card games)

--- src/data/Games/Spades.ts
+++ src/data/Games/Spades.ts
@@ -107,4 +107,71 @@ export const SpadesGames: GameData[] = [
         "One thing Heiya did was use a hot water pipe to cauterize a wound she received from when she got impaled by metal wiring. Hydration was key in this game. There are essentially no sage ways to collect water, so the only thing that could be done was to minimize water loss. Sweat should not be wiped down, or else the cooling process would take longer. Breathing deeply and slower cools the body down too. Heiya climbed through an air duct, but when she emerged, she decided to jump down since it was a dead end. This choice was reckless, and resulted in her losing her leg. What she could've done was climb on top of the vent instead of jumping down. To keep track of her direction, she could've also looked at the blood trail that she was leaving.",
     },
   },
+  {
+    id: "JS",
+    cardName: "Jack of Spades",
+    players: ["Kuina"],
+    description: {
+      registration: {
+        playerLimit: "None",
+        timeLimit: "None",
+      },
+      rules: [
+        "Players must defend themselves against the Jack of Spades and his crew in a physical brawl",
+        "Players may not bring in firearms into the venue but weapons such as pipes, machetes, and knives are provided",
+        "It is GAME OVER if the player dies or succumbs to their wounds",
+        "It is GAME CLEAR if the players successfully defeat the Jack of Spades and his crew",
+      ],
+      details: "This game takes place in the same building as the Jack of Clubs game.",
+    },
+  },
+  {
+    id: "QS",
+    cardName: "Queen of Spades",
+    name: "Checkmate",
+    players: ["Arisu", "Usagi"],
+    additionalPlayers: ["Kota"],
+    citizens: ["Risa - Queen of Spades"],
+    description: {
+      registration: {
+        playerLimit: 20,
+        timeLimit: "5 minutes per round",
+      },
+      rules: [
+        "The players, called the Challenger Team, will face the Queen Team. Each team has a King, both selected by the Queen of Spades",
+        "Each player has a vest with a button on its back. The Challenger Team's vests will glow blue and the Queen Team red",
+        "The game has 16 rounds with each team taking turns for 5 minutes per round. During their turn, the respective team must chase the other team and press the buttons on their vest. If a player's button is pressed, that player receives an electric shock that renders them unable to move for the rest of the turn. The next round, that player will become a member of the other team",
+        "The King of each team is excluded of team change (is bound to one team for the entire game)",
+        "Weapons are not permitted, but any hand-to-hand combat is allowed",
+        "After all 16 rounds, the team with the most members receives a GAME CLEAR and the team with fewer members receives a GAME OVER",
+      ],
+      details:
+        "This game is the only game where players can be temporary citizens as if the Queen Team wins, all players on it are allowed to treat themselves as citizens and continue to play alongside the Queen of Spades.",
+      solution:
+        "Similar to a typical Clubs game, cooperation is also vital for this game. However, with players constantly changing teams, keeping their loyalty is extremely difficult since players can be easily persuaded to join the other team in order to survive. Given the narrowness of the game venue, staying in a group when it is the opponent's turn to catch you is disadvantageous and it is considered better to separate and keep moving to avoid being captured. Attacking a member of the other team to prevent them from capturing a teammate is possible but risky. Forming small groups of two or three to chase a single opponent is a good move in order to capture them.",
+    },
+  },
+  {
+    id: "KS",
+    cardName: "King of Spades",
+    name: "Survival",
+    players: ["Arisu", "Usagi", "Kuina", "Akane", "Aguni", "An", "Chishiya", "Niragi"],
+    additionalPlayers: ["Every remaining player not currently within another game"],
+    citizens: ["Isao Shirabi - King of Spades"],
+    description: {
+      registration: {
+        playerLimit: "None",
+        timeLimit: "None",
+      },
+      rules: [
+        "The King of Spades is free to roam the entirety of the Borderlands, has access to a vast arsenal of weapons, knows the position of every player and will kill players by any means necessary",
+        "If a player kills the King of Spades, it's GAME CLEAR",
+        "If all players are killed by the King of Spades, it's GAME OVER",
+      ],
+      details:
+        "Out of all the games in the Borderlands, the King of Spades game had the most number of players and the highest death count.",
+      solution:
+        "Akane destroyed the King of Spades' tracker in order to lure him into an explosive trap, devised by Arisu, within the tight confines of a shop. Open entering the shop, Aguni charges at the King of Spades whilst he is offguard and despite being sprayed by bullets was able to push the King of Spades within the blast radius of the explosion and out the window of the shop. Aguni then finished him off or put him out of his misery caused by his injuries (that had been inflicted from the explosion).",
+    },
+  },
 ];
