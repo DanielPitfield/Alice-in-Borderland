a2b1ff1d34b60c047ced45eb859a8fec63f98a4e
Daniel Pitfield <danielpitfield1@gmail.com>
2025-02-14 12:20:48 +0000

    Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import "./globals.scss";
 
+import type { Metadata } from "next";
 import Navbar from "../components/Navbar";
+
+export const metadata: Metadata = {
+  title: "Alice in Borderland",
+  description:
+    "A website for everything Alice in Borderland (the TV show) including profile pages for all the main characters and all the card games!",
+  icons: { icon: "/favicon.ico" },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import styles from "../styles/index.module.scss";
 
-import Head from "next/head";
 import Image from "next/image";
 import BannerImage from "../../public/Images/Banner.jpg";
 import SocialMediaButton from "../components/SocialMediaButton";
@@ -8,30 +7,22 @@ import { SocialMediaNames } from "../data/SocialMedia";
 
 export default async function Page() {
   return (
-    <>
-      <Head>
-        <title>Alice in Borderland</title>
-        <meta name="description" content="A website for everything Alice in Borderland (the TV show)" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <section className={styles.container}>
+      <aside className={styles.navigation}>
+        <div className={styles.wrapper}>
+          <p className={styles.description}>All the trivia from the hit Netflix adaptation Alice in Borderland!</p>
 
-      <section className={styles.container}>
-        <aside className={styles.navigation}>
-          <div className={styles.wrapper}>
-            <p className={styles.description}>All the trivia from the hit Netflix adaptation Alice in Borderland!</p>
-
-            <ul className={styles.list}>
-              {SocialMediaNames.map((name) => (
-                <SocialMediaButton key={name} name={name} />
-              ))}
-            </ul>
-          </div>
-        </aside>
-
-        <div className={styles.imageWrapper}>
-          <Image src={BannerImage} alt="Banner" priority fill />
+          <ul className={styles.list}>
+            {SocialMediaNames.map((name) => (
+              <SocialMediaButton key={name} name={name} />
+            ))}
+          </ul>
         </div>
-      </section>
-    </>
+      </aside>
+
+      <div className={styles.imageWrapper}>
+        <Image src={BannerImage} alt="Banner" priority fill />
+      </div>
+    </section>
   );
 }
