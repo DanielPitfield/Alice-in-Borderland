e86a32f63898a3d7b8f2ac13e5204506cca1b00f
Daniel Pitfield <danielpitfield1@gmail.com>
2025-02-26 16:17:00 +0000

    Panel/lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,57 @@
 import styles from "../styles/index.module.scss";
 
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import BannerImage from "../../public/Images/Banner.jpg";
-import SocialMediaButton from "../components/SocialMediaButton";
-import { SocialMediaNames } from "../data/SocialMedia";
+import { CardSuites } from "../data/Cards/AllCards";
+import Link from "next/link";
+import { People } from "../data/People/AllPeople";
+import { PersonImageMappings } from "../data/People/PersonImageMappings";
 
 export default async function Page() {
   return (
     <section className={styles.container}>
       <aside className={styles.navigation}>
         <div className={styles.wrapper}>
-          <p className={styles.description}>All the trivia from the hit Netflix adaptation Alice in Borderland!</p>
+          <section className={styles.panel}>
+            <ul>
+              {CardSuites.map((suite) => (
+                <li key={suite}>{suite}</li>
+              ))}
+            </ul>
+          </section>
 
-          <ul className={styles.list}>
-            {SocialMediaNames.map((name) => (
-              <SocialMediaButton key={name} name={name} />
-            ))}
-          </ul>
+          <section className={styles.panel}>
+            <ul>
+              {People.map((person) => {
+                const image: StaticImageData | undefined = PersonImageMappings.find(
+                  (x) => x.name === person.name
+                )?.image;
+
+                return (
+                  <Link key={person.id} href={`/people/${person.name}`}>
+                    <li>
+                      <div className={styles.imageWrapper}>
+                        <Image
+                          className={styles.image}
+                          src={image ?? ""}
+                          alt={person.name}
+                          priority
+                          width={60}
+                          height={60}
+                        />
+                      </div>
+
+                      {person.name}
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+          </section>
         </div>
       </aside>
 
-      <div className={styles.imageWrapper}>
+      <div className={styles.banner}>
         <Image src={BannerImage} alt="Banner" priority fill />
       </div>
     </section>
